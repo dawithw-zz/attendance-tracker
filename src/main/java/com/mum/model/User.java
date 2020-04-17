0b253a965adc98409fb0eac963bfa123ae16c7b5
Dawit Woldegiorgis <dawithw@gmail.com>
2020-04-16 19:17:39 -0500

    updated source files to original versions

---
+++ src/main/java/com/mum/model/User.java
@@ -0,0 +1,112 @@
+package com.mum.model;
+
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.validation.Valid;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+
+@Entity
+@Table(name = "user")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "user_id")
+	private int id;
+	@Column(name = "email")
+	@Email(message = "{Email.err}")
+	@NotEmpty(message = "{NotEmpty.err}")
+	private String email;
+	@Column(name = "password")
+	@Length(min = 5, message = "{Length.err}")
+	@NotEmpty(message = "{NotEmpty.err}")
+	private String password;
+	@Column(name = "firstName")
+	@NotEmpty(message = "{NotEmpty.err}")
+	private String firstName;
+	@Column(name = "lastName")
+	@NotEmpty(message = "{NotEmpty.err}")
+	private String lastName;
+	@Column(name = "active")
+	private int active;
+	
+	@Valid
+	@ManyToMany(cascade = CascadeType.ALL)
+	@JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getActive() {
+		return active;
+	}
+
+	public void setActive(int active) {
+		this.active = active;
+	}
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+
+}
